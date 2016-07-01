9b0a416beb1567c729080c2f10b59db5ca1fd18e
Agustin Casal <sugusc21@gmail.com>
2016-07-01 19:41:49 -0300

    primero

---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+  end
+
+  test "should get product" do
+    get :product
+    assert_response :success
+  end
+
+end
